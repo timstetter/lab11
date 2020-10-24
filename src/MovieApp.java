95b929efa9bb28ded176facc8c3714bdab258586
timstetter <tstetter@gmail.com>
2020-10-24 13:35:08 -0400

    Added printMenu, switch for option choice, deleted Validator

--- src/MovieApp.java
+++ src/MovieApp.java
@@ -8,6 +8,7 @@ public class MovieApp {
 
 	static Set<String> categories = new HashSet<>();
 	static List<Movie> movies = new ArrayList<>();
+	static List<String> categoriesList;
 
 	public static void main(String[] args) {
 		Scanner scnr = new Scanner(System.in);
@@ -19,27 +20,84 @@ public class MovieApp {
 		for (Movie movie : movies) {
 			categories.add(movie.getCategory());
 		}
+		categoriesList = new ArrayList<>(categories.size());
+		for (String x : categories) {
+			categoriesList.add(x);
+		}
 
-		System.out.println("Welcome to the Movie List applcation");
-		System.out.println("There are " + movies.size() + " movies in the list");
+		System.out.println("Welcome to the Movie List application!\n");
+		System.out.println("There are " + movies.size() + " movies in the list\n");
+
+		int menuChoice = -1;
 		
-
-		do {
-
-			String userCategory = Validator.getString(scnr, "What category are you interested in? \n" + categories.toString() + ": ");
-
-			for (Movie movie : movies) {
-				if (userCategory.equalsIgnoreCase(movie.getCategory())) {
-					System.out.println(movie.getTitle());					
-				} 
-				
+		printMenu();
+		
+		menuChoice = scnr.nextInt();
+		
+		while (menuChoice != categoriesList.size() + 1) {
+			switch (menuChoice) {
+			case 1:
+				for (Movie movie : movies) {
+					if (movie.getCategory().equals("drama")) {
+						System.out.println(movie.getTitle());
+					}
+				}
+				break;
+			case 2:
+				for (Movie movie : movies) {
+					if (movie.getCategory().equals("horror")) {
+						System.out.println(movie.getTitle());
+					}
+				}
+				break;
+			case 3:
+				for (Movie movie : movies) {
+					if (movie.getCategory().equals("musical")) {
+						System.out.println(movie.getTitle());
+					}
+				}
+				break;
+			case 4:
+				for (Movie movie : movies) {
+					if (movie.getCategory().equals("comedy")) {
+						System.out.println(movie.getTitle());
+					}
+				}
+				break;
+			case 5:
+				for (Movie movie : movies) {
+					if (movie.getCategory().equals("animated")) {
+						System.out.println(movie.getTitle());
+					}
+				}
+				break;
+			case 6:
+				for (Movie movie : movies) {
+					if (movie.getCategory().equals("scifi")) {
+						System.out.println(movie.getTitle());
+					}
+				}
+				break;
+			default:
+				System.out.println("***** Please choose a correct menu option *****\n");
 			}
-			
 
-		} while (Validator.getYesNo(scnr, "Continue? (y/n)"));
+			printMenu();
+			menuChoice = scnr.nextInt();
+
+		}
+		scnr.close();
 
 		System.out.println("Thanks for using the Movies App!");
 
+	}
+
+	public static void printMenu() {
+		System.out.println("Please choose a category to learn more:");
+		for (int i = 0; i < categoriesList.size(); i++) {
+			System.out.print((i + 1) + ") " + categoriesList.get(i) + "\n");
+		}
+		System.out.println(categoriesList.size() + 1 + ") Exit");
 	}
 
 }
